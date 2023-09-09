cfb8164ccaa668b517c8a0a53184049e4e75a799
Chamith Lakshan <70762572+ClaZieR@users.noreply.github.com>
2023-09-09 22:55:17 +0530

    button dis and loading -php

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,46 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
-
 
 function App() {
+  const [output, setOutput] = useState("");
+  const [apikey, setApikey] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // Initially, the button is not disabled
 
-  
-  const [output, setOutput] = useState("")
-  const [apikey, setApikey] = useState("")
-  
-  function handle(event){
-    setApikey(event.target.value)
+  function handle(event) {
+    setApikey(event.target.value);
   }
 
   const getitfrombackent = () => {
+    setIsButtonDisabled(true);
+    setOutput("Loading") // Disable the button when the request starts
 
-    const option = {
-
+    const options = {
       method: 'GET',
       url: 'http://localhost:8080/',
       params: { apikey: apikey },
-    }
+    };
 
-    axios.request(option).then(function (response) {
-      console.log(response.data);
-      setOutput(response.data)
-    }).catch(function (error) {
-      console.error(error);
-    });
+    axios.request(options)
+      .then(function (response) {
+        setOutput(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      })
+      .finally(function () {
+        setIsButtonDisabled(false); // Enable the button when the request is complete (success or error)
+      });
   }
 
-  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to 2stepREP</h1>
         <input type="text" placeholder="Enter API Key" onChange={handle}></input>
-        <button onClick={getitfrombackent}>Get it from backend</button>
+        <button onClick={getitfrombackent} disabled={isButtonDisabled}>
+          Get it from backend
+        </button>
         <h1>{output}</h1>
       </header>
     </div>
